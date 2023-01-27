42911d5877057bbfb809b14eb89a2f5f4e9bbd50
Brooks Palin <brookspalin710@gmail.com>
2023-01-26 17:19:22 -0700

    More work on top level lexing

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,14 +3,38 @@ use logos::Logos;
 #[derive(Debug, Logos)]
 enum LibrettoToken {
 
-  #[regex("(\"[^\"]*\")")]
+  #[regex("#([^ \t\n]*)")]
+  Tag,
+  
+  #[regex(":([^ \t\n]*)")]
+  Speaker,
+
+  #[regex("<([^><]*)>")]
+  Logic,
+
+  #[regex("\"([^\"]*)\"")]
   Quote,
 
   #[token("|")]
   Bar,
 
+  #[token("{")]
+  LeftCurlyBracket,
+
+  #[token("}")]
+  RightCurlyBracket,
+
+  #[token("->")]
+  Arrow,
+
+  #[token("--")]
+  Dash,
+
   #[token("request")]
   Request,
+
+  #[regex(r"//[^\n\r]+(?:\*\)|[\n\r])", logos::skip)]
+  Comment,
 
   #[regex(r"[ \t\n\f]+", logos::skip)]
   Whitespace,
